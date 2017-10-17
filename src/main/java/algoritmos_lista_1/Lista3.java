a6516cff93c4f348e49d33baa6db8f8243e3f0f3
Wellington <wellington.avelino@outlook.com>
2017-10-17 17:50:42 -0200

    Adding new exercicies to loops

--- src/main/java/algoritmos_lista_1/Lista3.java
+++ src/main/java/algoritmos_lista_1/Lista3.java
@@ -78,4 +78,92 @@ public class Lista3 {
 
         System.out.println(name[ageIndex] + " " + age[ageIndex] + " " + gender[ageIndex]);
     }
+
+    public static void printMultiplyMinorof200(){
+        int count = 0;
+        while (count <= 200){
+            if ((count % 7) == 0){
+                System.out.println(count + " is a multiply of 7");
+            }
+            count++;
+        }
+    }
+
+    public static void calculateArithimetcAverage(){
+        int grades = 0;
+        int quantity = 0;
+        for (int i = 13; i <= 73 ; i++) {
+            if((i % 2) == 0 ){
+                quantity++;
+                grades = grades + i;
+            }
+        }
+        System.out.println(grades/quantity);
+    }
+
+    public static void calculateClassArithimeticAverage(){
+        int studentQuantity = scan.nextInt();
+        double arithimeticAverage = 0;
+        int count = 0;
+        while (count < studentQuantity){
+            System.out.println("Insert the grade student");
+            int classGrades = scan.nextInt();
+
+            arithimeticAverage = arithimeticAverage + classGrades;
+            count++;
+        }
+        System.out.println("The arithimetic average is" + arithimeticAverage/count);
+    }
+
+    public static void fractionSum(){
+        double sum = 0;
+        for (int i = 2; i <= 20; i++) {
+            sum +=  1f/i;
+        }
+        System.out.printf("%.2f",sum  + 1);
+    }
+
+    public static void fractionSumWithInput(){
+        int number = scan.nextInt();
+        double sum = 1;
+        for (int i = 2; i <= number; i++) {
+            if((i % 2)== 0){
+                sum -=  1f/i;
+            }else
+                sum += 1f/i;
+        }
+        System.out.printf("%.2f",sum);
+    }
+
+    //TODO fazer o exercicio 12
+
+    public static void naturalSum() {
+        int number = scan.nextInt();
+        float sum = 0;
+        for (int k = 2; k <= number; k++) {
+            int expoent = 1;
+            int base = 1;
+
+            for (int i = 2, j = 3; i <= k; i++, j += 2) {
+                expoent *= i;
+                base *= j;
+            }
+            sum += (float) expoent / (float) base;
+        }
+
+        System.out.printf("%.2f",sum + 1);
+    }
+
+    public static void calculateNaturalNumber(){
+        int number = scan.nextInt();
+        float sum = 0;
+        for (int k = 2; k <= number; k++) {
+
+            for (int i = 2, j = 3; i <= k; i += 2, j += 2) {
+                sum += (float) i / (float) j;
+            }
+        }
+        System.out.printf("%.2f",sum + 1);
+    }
+
 }
